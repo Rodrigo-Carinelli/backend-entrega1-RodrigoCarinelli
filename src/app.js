fdb0bf31024c9ae573eceeefd900dfd61aaafab5
Rodrigo-Carinelli <rodrigo.carinelli@hotmail.com>
2025-09-20 23:40:20 -0300

    terminando el tp

--- src/app.js
+++ src/app.js
@@ -31,44 +31,44 @@ import ProductManager from './managers/ProductManager.js';
 const app = express();
 const productManager = new ProductManager('./src/data/products.json');
 
-// Middlewares
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public')); // archivos estáticos (JS del frontend)
+app.use(express.static('public')); 
 
-// Handlebars
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './src/views');
 
-// Rutas API
+
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 app.use('/', viewsRouter);
 
-// Servidor
+
 const PORT = 8080;
 const httpServer = app.listen(PORT, () => {
   console.log(`✅ Servidor escuchando en http://localhost:${PORT}`);
 });
 
-// Socket.io
+
 const io = new Server(httpServer);
 io.on('connection', async (socket) => {
   console.log('🟢 Cliente conectado');
 
-  // enviar productos al conectarse
+  
   const products = await productManager.getProducts();
   socket.emit('productList', products);
 
-  // escuchar producto nuevo
+  
   socket.on('newProduct', async (data) => {
     await productManager.addProduct(data);
     const updatedProducts = await productManager.getProducts();
     io.emit('productList', updatedProducts);
   });
 
-  // escuchar eliminación de producto
+  
   socket.on('deleteProduct', async (id) => {
     await productManager.deleteProduct(id);
     const updatedProducts = await productManager.getProducts();
